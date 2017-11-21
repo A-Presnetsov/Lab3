a4136fe06e997300c417577402cc1a032c58dade
A-Presnetsov <33604502+A-Presnetsov@users.noreply.github.com>
2017-11-22 00:54:03 +0300

    Add files via upload

---
+++ Lab3/Lab3 - client/Lab3 - client/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab3___client
+{
+    class Program
+    {
+        static void Main(string[] args) {
+            while (true) {
+                try {
+                    Console.Write("Введите строку: ");
+                    string str = Console.ReadLine();
+                    TcpClient socket = new TcpClient("192.168.0.103", 1918);
+                    NetworkStream nwStream = socket.GetStream();
+                    BinaryReader reader = new BinaryReader(nwStream);
+                    BinaryWriter writer = new BinaryWriter(nwStream);
+                    writer.Write(Encoding.UTF8.GetByteCount(str)); 
+                    writer.Write(str); 
+
+                    int length = reader.ReadInt32(); 
+                    string response = reader.ReadString();
+                    if (Encoding.UTF8.GetByteCount(response) != length)
+                        Console.WriteLine("Передача данных не удалась");
+                    else
+                        Console.WriteLine(response);
+                }
+                catch (Exception e) { Console.WriteLine(e.Message); }
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Lab3/Lab3 - server/Lab3 - server/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab3_server
+{
+    class Program
+    {
+        static void Main(string[] args) {
+            TcpListener serverSocket = new TcpListener(1918);
+            serverSocket.Start();
+
+            Console.WriteLine("Сервер запущен");
+
+            while (true) {
+                try {
+                    TcpClient socket = serverSocket.AcceptTcpClient();
+                    NetworkStream nwStream = socket.GetStream();
+                    BinaryReader reader = new BinaryReader(nwStream);
+                    BinaryWriter writer = new BinaryWriter(nwStream);
+                    int length = reader.ReadInt32(); 
+                    string str = reader.ReadString();
+                    Console.WriteLine("Клиент прислал строку: " + str);
+                    string response = Encoding.UTF8.GetByteCount(str) == length ? str.Length + " символов" : "Передача данных не удалась";
+
+
+                    writer.Write(Encoding.UTF8.GetByteCount(response)); 
+                    writer.Write(response); 
+                    socket.Close();
+                } catch(Exception e) { Console.WriteLine(e.Message); }
+            }
+            serverSocket.Stop();
+            Console.ReadKey();
+        }
+    }
+}
